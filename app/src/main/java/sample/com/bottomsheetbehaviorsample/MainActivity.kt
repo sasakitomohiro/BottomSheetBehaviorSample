3569f234d60fe61dfe34744069b3e9325df08622
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-06 16:46:09 +0900

    BottomSheetCallback実装

--- app/src/main/java/sample/com/bottomsheetbehaviorsample/MainActivity.kt
+++ app/src/main/java/sample/com/bottomsheetbehaviorsample/MainActivity.kt
@@ -2,11 +2,25 @@ package sample.com.bottomsheetbehaviorsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomSheet = findViewById<View>(R.id.bottomSheetBehavior)
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(p0: View, p1: Float) {
+
+            }
+
+            override fun onStateChanged(p0: View, p1: Int) {
+
+            }
+        })
     }
 }
