96c236551f35bc2694051f723fb0c1e689052527
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-07 14:55:27 +0900

    create state change button

--- app/src/main/java/sample/com/bottomsheetbehaviorsample/MainActivity.kt
+++ app/src/main/java/sample/com/bottomsheetbehaviorsample/MainActivity.kt
@@ -3,6 +3,7 @@ package sample.com.bottomsheetbehaviorsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
         val bottomSheet = findViewById<View>(R.id.bottomSheetBehavior)
         val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        button1.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+        button2.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+        button3.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
         bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onSlide(p0: View, p1: Float) {
 
